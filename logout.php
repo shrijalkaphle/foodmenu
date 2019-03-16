52fdf2d09e7ba5caf611891ed4b76bab644ad3ab
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2019-03-16 18:51:36 +0545

    minor file/folder management

--- logout.php
+++ logout.php
@@ -3,10 +3,6 @@
 	$stat = $_GET[stat];
 	session_start();
 	session_destroy();
-
-	// if ($stat == 1) {
-	// 	header("location:index.php");
-	// }
 	header("location:index.php");
 	exit();
 ?>
--- order.php
+++ order.php
@@ -28,19 +28,6 @@
 
 	$query4 = "INSERT INTO order_details(tableno,price) VALUES ('$tno','$total')";
 	$result4 = mysqli_query($conn,$query4);
-
-	// $query4 = "SELECT * FROM order_details WHERE tableno = '$tno'";
-	// $result4 = mysqli_query($conn,$query4);
-	// $r1 = mysqli_fetch_assoc($result4);
-	// $n = mysqli_num_rows($result4);
-	// if($n != 0) {
-	// 	$price = $r1['price'];
-	// 	$total = $price + $total;
-	// 	$query5 = "INSERT INTO order_details(tableno,price) VALUES ('$tno','$total')";
-	// 	$result5 = mysqli_query($conn,$query5);
-	// } else {
-	// 	$query5 = "INSERT INTO order_details(tableno,price) VALUES ('$tno','$total')";
-	// 	$result5 = mysqli_query($conn,$query5);
 	}
 
 
